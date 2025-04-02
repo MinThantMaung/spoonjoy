68f98e4cc309f1a702e468f2877c689077301d97
MinThantMaung <minthant180@gmail.com>
2025-04-02 11:37:38 +0900

    feat:nav data

---
+++ utils/routes.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+    {label: 'Home' , href: '/'},
+    {label: 'Menu' , href: '/menu'},
+    {label: 'Country' , href: '/country'},
+    {label: 'Profile' , href: '/profile'},
+]
