ff659fb9942edd804438af891669ed19c4def6d0
MinThantMaung <minthant180@gmail.com>
2025-04-02 11:41:46 +0900

    feat:implement hero section in main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+'use client'
 import Nav from "@/app/components/_nav/page"
+import Hero from "@/app/components/hero/page"
 export default function Home() {
   return (
     <>
       <nav>
         <Nav />
       </nav>
+      <section>
+        <Hero />
+      </section>
     </>
   );
 }
