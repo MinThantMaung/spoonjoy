d50beb62565c5063833e514b2316ce095be4fa93
MinThantMaung <minthant180@gmail.com>
2025-04-02 11:37:05 +0900

    feat:implement nav page

---
+++ src/app/components/_nav/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+import React from 'react'
+import { navItems } from '../../../../utils/routes'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+
+const page = () => {
+    const pathname = usePathname()
+  return (
+    <>
+        <div className='flex justify-between items-center'>
+            <h1 className='p-4 font-bold'>Spoonjoy</h1>
+            <ul className='hidden sm:flex sm:space-x-5 sm:mr-5'>
+                {navItems.map(({label,href}) => (
+                    <li key={label}>
+                        <Link
+                            href={href}
+                            className={`cursor-pointer transition duration-200 hover:text-blue-500 underline ${
+                                pathname === href ? 'font-bold text-blue-600' : ''
+                            }`}
+                            >
+                        {label}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </>
+  )
+}
+
+export default page
