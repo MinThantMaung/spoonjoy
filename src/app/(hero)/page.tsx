8036d7c7ce0e9354de6e30aa3e8169bfe838695e
MinThantMaung <minthant180@gmail.com>
2025-04-03 13:17:55 +0900

    fix: ui responsive

--- src/app/(hero)/page.tsx
+++ src/app/(hero)/page.tsx
@@ -19,29 +19,33 @@ export default function Home() {
         <meta name="description" content="Explore delicious recipes crafted by chefs and cook confidently every day." />
       </Head>
       <section className="flex w-full">
-        <div className="flex flex-col items-center justify-center text-center space-y-3 px-4 py-12 mx-auto">
+        <div className="flex flex-col items-center justify-center text-center space-y-3 px-3 sm:px-4 py-12 mx-auto">
             {/* Heading */}
-          <header className="flex items-center gap-x-4 flex-wrap justify-center">
-            <span className="text-5xl font-extrabold">Explore recipes</span>
-            <Image src={think} alt="Illustration of a person thinking about food" width={100} height={100}/>
-            <span className="text-5xl font-extrabold">like a chef</span>
+          <header className="sm:flex items-center sm:gap-x-4 flex-wrap justify-center">
+            <span className="text-4xl sm:text-5xl font-extrabold">Explore recipes</span>
+            <div className="flex justify-center items-center">
+              <Image src={think} alt="Illustration of a person thinking about food" className="w-20 h-20 sm:h-36 sm:w-36 sm:flex justify-center items-center"/>
+            </div>
+            <span className="text-4xl sm:text-5xl font-extrabold">like a chef</span>
           </header>
 
           {/* Subheading */}
-          <header className="flex items-center gap-x-4 flex-wrap justify-center">
-            <span className="text-4xl font-semibold">Cook confidently</span>
-            <Image src={cooking} alt="Illustration of someone cooking" width={100} height={100}/>
-            <span className="text-4xl font-semibold">every day</span>
+          <header className="sm:flex items-center gap-x-4 flex-wrap justify-center">
+            <span className="text-xl sm:text-4xl font-semibold">Cook confidently</span>
+            <div className="flex justify-center items-center">
+              <Image src={cooking} alt="Illustration of someone cooking" width={100} height={100}/>
+            </div>
+            <span className="text-xl sm:text-4xl font-semibold">every day</span>
           </header>
 
           {/* Description */}
-          <div className="text-gray-600 text-lg">
+          <div className="text-gray-600 text-sm sm:text-lg">
             <p>Crafted by chefs, inspired by global flavors.</p>
             <p>From discovery to dining — all in a single tap.</p>
           </div>
 
           {/* Search bar */}
-          <div className="w-full mt-10">
+          <div className="w-full mt-4 sm:mt-10">
             <SearchBar onSearch={handleSearch} />
           </div>
 
