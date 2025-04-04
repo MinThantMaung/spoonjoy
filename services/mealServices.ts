17c522c37e66b824b85b8cfb499f9b1a994ca276
MinThantMaung <minthant180@gmail.com>
2025-04-04 17:44:56 +0900

    feat:add mealbyarea route

--- services/mealServices.ts
+++ services/mealServices.ts
@@ -4,6 +4,10 @@ export const fetchMealsByCategory = (category: string) => {
     return axiosInstance.get(`filter.php?c=${category}`);
 };
 
+export const fetchMealsByArea = (area: string) => {
+    return axiosInstance.get(`filter.php?a=${area}`);
+};
+
 export const fetchAllArea = () => {
     return axiosInstance.get(`list.php?a=list`);
 };
