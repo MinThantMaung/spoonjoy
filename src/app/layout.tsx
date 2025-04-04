8e3595b225ba6ae1b9691e7e7bee1684d2e14bb2
MinThantMaung <minthant180@gmail.com>
2025-04-04 10:59:33 +0900

    fix:wrap with provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "@/app/components/_nav/page"
+import Providers from "./provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
         <nav>
           <Nav />
         </nav>
-        {children}
+        <Providers>
+          {children}    
+        </Providers>
       </body>
     </html>
   );
