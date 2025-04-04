14ff4e2a9b688aad258fe7d2d099875c98f8ebc4
MinThantMaung <minthant180@gmail.com>
2025-04-04 13:34:46 +0900

    feat:add type

---
+++ types/meal.ts
@@ -0,0 +1,14 @@
+export interface Meal {
+    idMeal: string;
+    strMeal: string;
+    strMealThumb: string;
+  }
+  
+  export interface Area {
+    strArea: string;
+  }
+  
+  export interface Category {
+    strCategory: string;
+  }
+  
