5c8b3852598f06d385f8bada0733410f07fc6b1a
MinThantMaung <minthant180@gmail.com>
2025-04-04 13:34:29 +0900

    fix:update image

--- src/app/components/ui/MenuCard.tsx
+++ src/app/components/ui/MenuCard.tsx
@@ -1,31 +1,38 @@
 import {
     Card,
     CardContent,
-    CardDescription,
     CardFooter,
     CardHeader,
     CardTitle
   } from "@/components/ui/card"
   import { Button } from "@/components/ui/button"
   import { ArrowRight } from "lucide-react"
+import Image from "next/image";
+  
+  type Props = {
+    title: string;
+    image: string;
+  };
 
-const MenuCard = ({ title, description, image }: { title: string, description: string, image: string }) => (
+const MenuCard = ({ title, image }: Props) => (
     <Card className="overflow-hidden border-none shadow-lg transition-transform hover:scale-[1.02]">
       <CardHeader className="pb-0">
         <div className="flex justify-between items-start">
           <div>
-            <CardTitle className="text-xl font-bold tracking-tight">{title}</CardTitle>
-            <CardDescription className="mt-1">{description}</CardDescription>
+            <CardTitle className="text-xl font-bold tracking-tight h-8 sm:h-14">{title}</CardTitle>
           </div>
         </div>
       </CardHeader>
       <CardContent>
-        <div className="relative h-32 overflow-hidden rounded-lg">
+        <div className="relative h-44 sm:h-32 overflow-hidden rounded-lg">
           <div className="absolute inset-0 bg-gradient-to-br from-violet-500/10 to-pink-500/10" />
-          <img
+          <Image
             src={image}
             alt={title}
+            width={100}
+            height={100}
             className="object-cover w-full h-full transition-transform duration-500 hover:scale-105"
+            priority
           />
         </div>
       </CardContent>
