86365639e76fb52f714472e197c9ffa2aefbb4ad
MinThantMaung <minthant180@gmail.com>
2025-04-07 15:58:29 +0900

    feat:rename pagination

--- src/app/(menu)/menu/page.tsx
+++ src/app/(menu)/menu/page.tsx
@@ -18,11 +18,13 @@ import { Skeleton } from "@/components/ui/skeleton";
 import Loading from "@/app/components/ui/Loading";
 import ErrorMessage from "@/app/components/ui/ErrorMessage";
 import EmptyState from "@/app/components/ui/EmptyState";
-import PaginationComponent from "@/app/components/common/pagination";
+import PaginationComponent from "@/app/components/common/FilterPagination";
 
 export default function MenuPage() {
   const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
   const [selectedArea, setSelectedArea] = useState<Area | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const mealsPerPage = 10;
 
   // Get filter options
   const { data: areas = [], isLoading: isAreasLoading } = useFindByArea();
@@ -58,55 +60,69 @@ export default function MenuPage() {
     isLoading = isMealsCategoryLoading;
   }
 
+  //pagination page handle
+  const totalPages = Math.ceil(mealsToShow.length / mealsPerPage);
+  const startIndex = (currentPage - 1) * mealsPerPage;
+  const endIndex = startIndex + mealsPerPage;
+  const currentMeals = mealsToShow.slice(startIndex, endIndex);
+
   return (
     <>
-      <NavigationMenu className="my-4 mx-6">
-        <NavigationMenuList>
-          {isAreasLoading ? (
-            <Skeleton className="h-8 w-[75px] rounded-sm bg-white" />
-          ) : (
-            <FilterDropdown<Area>
-              label="Area"
-              items={areas}
-              getTitle={(a) => a.strArea}
-              selected={selectedArea}
-              onSelect={(a) => setSelectedArea(a)}
+      <div className="flex flex-col">
+        <NavigationMenu className="my-4 mx-6">
+          <NavigationMenuList>
+            {isAreasLoading ? (
+              <Skeleton className="h-8 w-[75px] rounded-sm bg-white" />
+            ) : (
+              <FilterDropdown<Area>
+                label="Area"
+                items={areas}
+                getTitle={(a) => a.strArea}
+                selected={selectedArea}
+                onSelect={(a) => setSelectedArea(a)}
+              />
+            )}
+            {isCategoriesLoading ? (
+              <Skeleton className="h-8 w-[100px] rounded-sm bg-white" />
+            ) : (
+              <FilterDropdown<Category>
+                label="Category"
+                items={categories}
+                getTitle={(c) => c.strCategory}
+                selected={selectedCategory}
+                onSelect={(c) => setSelectedCategory(c)}
+              />
+            )}
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        {/* Meals Grid */}
+        <div className="mx-6 my-4 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-6">
+          {isLoading &&
+            Array.from({ length: 10 }).map((_, idx) => <Loading key={idx} />)}
+
+          {!isLoading && isMealsCategoryError && <ErrorMessage />}
+
+          {!isLoading && mealsToShow.length === 0 && <EmptyState />}
+
+          {!isLoading &&
+            currentMeals.map((meal: Meal) => (
+              <MenuCard
+                key={meal.idMeal}
+                title={meal.strMeal}
+                image={meal.strMealThumb}
+              />
+            ))}
+        </div>
+        {mealsToShow.length > 0 && (
+          <div className="mt-auto pt-4 pb-6">
+            <PaginationComponent
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={(page) => setCurrentPage(page)}
             />
-          )}
-          {isCategoriesLoading ? (
-            <Skeleton className="h-8 w-[100px] rounded-sm bg-white" />
-          ) : (
-            <FilterDropdown<Category>
-              label="Category"
-              items={categories}
-              getTitle={(c) => c.strCategory}
-              selected={selectedCategory}
-              onSelect={(c) => setSelectedCategory(c)}
-            />
-          )}
-        </NavigationMenuList>
-      </NavigationMenu>
-
-      {/* Meals Grid */}
-      <div className="mx-6 my-4 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-6">
-        {isLoading &&
-          Array.from({ length: 10 }).map((_, idx) => <Loading key={idx} />)}
-
-        {!isLoading && isMealsCategoryError && <ErrorMessage />}
-
-        {!isLoading && mealsToShow.length === 0 && <EmptyState />}
-
-        {!isLoading &&
-          mealsToShow.slice(0, 10).map((meal: Meal) => (
-            <MenuCard
-              key={meal.idMeal}
-              title={meal.strMeal}
-              image={meal.strMealThumb}
-            />
-          ))}
-      </div>
-      <div className="py-6">
-        <PaginationComponent />
+          </div>
+        )}
       </div>
     </>
   );
