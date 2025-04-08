bf3c59fa6d8baf712252428d367e4142d1c74149
MinThantMaung <minthant180@gmail.com>
2025-04-08 11:50:24 +0900

    fix:temp close for profile

--- utils/routes.ts
+++ utils/routes.ts
@@ -2,5 +2,5 @@ export const navItems = [
     {label: 'Home' , href: '/'},
     {label: 'Menu' , href: '/menu'},
     {label: 'Country' , href: '/country'},
-    {label: 'Profile' , href: '/profile'},
+    // {label: 'Profile' , href: '/profile'},
 ]
