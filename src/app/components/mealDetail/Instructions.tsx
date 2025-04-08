0bdb9c3b4dd97040a2124b85c703b011c5a1dd21
MinThantMaung <minthant180@gmail.com>
2025-04-08 16:56:41 +0900

    feat:create instruction

---
+++ src/app/components/mealDetail/Instructions.tsx
@@ -0,0 +1,17 @@
+type Props = {
+    text: string;
+  };
+  
+  export default function Instructions({ text }: Props) {
+    return (
+      <div className="flex flex-col justify-center items-center my-8">
+        <div className="max-w-5xl ml-4 mr-4 sm:ml-0 sm:mr-0">
+          <h2 className="text-xl font-semibold mt-6 mb-3">Instructions</h2>
+          <div className="bg-white text-black p-6 rounded-xl shadow border border-zinc-200 leading-relaxed whitespace-pre-line">
+            {text}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
