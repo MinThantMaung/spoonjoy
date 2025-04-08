ea1151bf8be81e6c20c30649497de36c5cfdb66d
MinThantMaung <minthant180@gmail.com>
2025-04-08 13:37:01 +0900

    fix:handle nav

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,21 +1,33 @@
-"use client"
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {PropsWithChildren} from "react";
+"use client";
 
-const Providers = ({children}: PropsWithChildren<object>) => {
-    const queryClient = new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 5 * 60 * 1000,
-            refetchOnWindowFocus: false,
-          }
-        }
-    });
-    return <QueryClientProvider client={queryClient}>
-        <div>
-            {children}
-        </div>
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+import { usePathname } from "next/navigation";
+import Nav from "@/app/components/_nav/page";
+
+const Providers = ({ children }: PropsWithChildren<object>) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
+  const pathname = usePathname();
+  const hideNav = pathname.startsWith("/menu/") && pathname.split("/").length === 3;
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {!hideNav && (
+        <nav>
+          <Nav />
+        </nav>
+      )}
+      <div>{children}</div>
     </QueryClientProvider>
-}
+  );
+};
 
-export default Providers
+export default Providers;
